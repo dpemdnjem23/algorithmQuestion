22551dc9d718fc813f798018d9576c3017716926
dpemdnjem23 <83865101+dpemdnjem23@users.noreply.github.com>
2023-08-03 00:12:00 +0900

    [unrated] Title: 과제 진행하기, Time: 2.53 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.js
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.js
@@ -1,42 +1,101 @@
-function timeToMin(time) {
-  const [hh, mm] = time.split(':').map(Number);
-  return hh * 60 + mm;
-}
-
 function solution(plans) {
-  const answer = [];
+    var answer = [];
+    
+    
+    // 과제를 시작하면 배열에 넣는다.ㄱ
+    // 시간을 분으로 바꿔 계산을 용이하게 한다.
+    
+    //plans를 먼저 바꿔준다.
+    
+        //converTime으로 계산이 편하게 분으로 변환 함수
+        const converTime = (num) =>{
+        //[a,b]의 형태
+        const [a,b] = num.split(':')
+        
+        return Number(a)*60+Number(b)
 
-  const sortedPlans = plans
-    .map(([subject, time, count]) => [subject, timeToMin(time), Number(count)])
-    .sort((a, b) => a[1] - b[1]);
-
-  const stack = [];
-
-  for (let i = 0; i < sortedPlans.length - 1; i++) {
-    const [subject, time, count] = sortedPlans[i];
-
-    if (time + count <= sortedPlans[i + 1][1]) {
-      answer.push(subject);
-      let availableTime = sortedPlans[i + 1][1] - time - count;
-
-      while (stack.length) {
-        const [currentSubject, currentTime] = stack.pop();
-        if (currentTime <= availableTime) {
-          availableTime -= currentTime;
-          answer.push(currentSubject);
-        } else {
-          stack.push([currentSubject, currentTime - availableTime]);
-          break;
-        }
-      }
-    } else {
-      stack.push([subject, count - (sortedPlans[i + 1][1] - time)]);
+        
     }
-  }
-  answer.push(sortedPlans[sortedPlans.length - 1][0]);
+
+    //분으로 변환해서 plans에 삽입
+    for(let i =0 ; i<plans.length;i++){
+      plans[i][1] =  converTime(plans[i][1]) 
+      plans[i][2] = Number(plans[i][2])
+    }
+
+    
+    //과제는 시작하기 한 시각 이 되면 시작한다 -> 시작시간 순서대로 정렬한다면 배열에서 순서대로 꺼내면된다.
+    
+    plans.sort((a,b)=>{
+        return a[1]-b[1]
+    })
+    
+    //새로운 과제를 시작할 시간되었을때, 기존에 진행 중이던 과제는 멈추고, 새로운 과제 시작
+    
+    // 사건이 일어날때의 시간만 기록한다.
+    
+    let currentTime = plans[0][1];
+    let stack = []
+    let end = 0
+            // [["music", "12:20", "40"],["computer", "12:30", "100"], ["science", "12:40", "50"], ["history", "14:00", "30"],  ]
+    
+    //music 시작 -> stack에 푸쉬 -> 다음 시간 계산 12:30-12:20  ,["music", "12:20", "30"],currentTime = 12:30,        
+        
+
+ 
+    for(let i = 0 ; i<plans.length-1;i++){
+        
+        const [name,time,remain] = plans[i]
+        const [name2,time2,remain2] = plans[i+1]
+        
+        //AAA가 12:00 에 stack에 들어감
+        //['aaa']
+        // aaa는 멈추고 bbb시작
+        // bbb에서 ccc
+        //1. while을 이용해서 stack을 모두 처리해야한다.
+        //2. while을 이용하는데 stack이 남아있는데 나머지가 처리되는경우
+        // 순서대로 딱딱 떨이지는 경우[["korean", "11:40", "30"], ["english", "12:10", "20"], ["math", "12:30", "40"]]
+        if(time+remain <= time2){
+            answer.push(name)
+            
+            currentTime =time2-time-remain
+                        
+            while(stack.length){
+                const [Pname,Ptime,Premain] = stack.pop()
+                //stack을 달고오는 경우중에 stack이 마지막에 처리 돼야하는경우
+                if(Premain<=currentTime){
+                    
+                   currentTime-=Premain
+                answer.push(Pname)
+                }
+                else{
+                    stack.push([Pname,Ptime,Premain-currentTime])
+                    break;
+                }
+                
+                
+            }
+            
+            
+        }
+        else{
+            
+            stack.push([name,time,remain-(time2-time)])
+            
+            
+            
+        }
+        
+      
+    
+        
+    }
+
+      answer.push(plans[plans.length - 1][0]);
 
   while (stack.length) {
     answer.push(stack.pop()[0]);
   }
-  return answer;
+
+    return answer;
 }
