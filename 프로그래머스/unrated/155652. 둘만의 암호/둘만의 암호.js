1f5e3151ff67b448b3636a8244b08976a3ebf168
dpemdnjem23 <83865101+dpemdnjem23@users.noreply.github.com>
2023-08-09 17:06:17 +0900

    [unrated] Title: 둘만의 암호, Time: 3.78 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,16 @@
+function solution(s, skip, index) {
+    var answer = '';
+    //알파벳을 나열하고 index만큼 세고
+    //skip이 있다면 건너뛰고 index를 차감시키지 않는다.
+    //알파벳 나열대신 charCodeAt , fromCharCode 사용
+    //a =97 b=98 c=99 z=122
+    //배열화를 한다.
+    
+    const arrS = [...s]
+    
+    console.log(arrS,'a'.charCodeAt())
+    
+    
+    
+    return answer;
+}
