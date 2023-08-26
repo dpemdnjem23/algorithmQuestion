e38e8934c69df8c1d19b400f46d4930880b05f17
dpemdnjem23 <83865101+dpemdnjem23@users.noreply.github.com>
2023-08-27 02:33:35 +0900

    [unrated] Title: 공원 산책, Time: 5.10 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
@@ -1,61 +1,78 @@
 function solution(park, routes) {
-    // 이전 위치
-    let prevPos = [0, 0]
+    var answer = [0,0];
+    let start = [0,0]
+    const prepos = [0,0]
+    // 공원 가로세로 길이
+  const maxRow = park.length - 1; // ->
+  const maxCol = park[0].length - 1; 
     
-    // 2차원 배열 지도
-    const map = []
-    park.forEach((row, idx) => {
-        const sPos = row.indexOf('S')
-        if(sPos >= 0) {
-            prevPos = [idx, sPos]
-        }
-        map.push([...row])
-    })
+    const dist = {E:[0,1],S:[1,0],W:[0,-1],N:[-1,0]}
     
-    // 사용자 방향 딕셔너리
-    const dict = {
-        E: [0, 1],
-        W: [0, -1],
-        S: [1, 0],
-        N: [-1, 0]
+    //장애물 판별
+    const validate = (row,col)=>{
+        
+        console.log(row,col,maxRow,maxCol)
+        return row>maxRow||row<0||col<0||col>maxCol
+        
     }
+    //지다다니는길 
+    //00
+    //routes에 따라 이동한다.
+    //e 오른쪽 s 아래쪽 w 왼쪽 n 위쪽
+    //routes를 통해 park를 순회 한다. 만약ㅌ
+    //만약 장애물을 지나거나, 밖으로 나가는경우는 이동하지않는다.
     
-    routes.forEach((route, routeIdx) => {
-        // 방향과 거리 입력받기
-        const [pos, range] = route.split(" ")
-        let curPos = [...prevPos]
-        let applyFlag = true
-        
-        // 거리만큼 반복
-        for(let i = 0 ; i < range; i ++) {
-            
-            curPos[0]+= dict[pos][0]
-            curPos[1]+= dict[pos][1]
-            
-            // 장애물 판별 || 공원을 벗어났는지 판별
-            if(
-                    (curPos[0] > map.length - 1) || 
-                    (curPos[0] < 0) ||
-                    (curPos[1] > map[0].length - 1) ||
-                    (curPos[1] < 0)
-    )
-            {
-                applyFlag = false
-                break
-            }
-            
-            if(map[curPos[0]][curPos[1]] === 'X') {
-                applyFlag = false
-                break
-            }
-            
+    //[0s0
+    // 000   
+    // 0x0 
+    // 000]    
+    
+    //시작점의 좌표를 잡아야한다.
+    park.forEach((el,idx)=>{
+        const st = el.indexOf('S')
+        if(st>-1){
+                    start=[idx,st]
+
         }
-        
-        if(applyFlag) prevPos = curPos
-    
     })
     
     
-    // 문제가 없다면 이 변수에 반영되도록
-    return prevPos
+    routes.forEach((route)=>{
+        const [dir,num]= route.split(' ')
+      
+        let curpos= [...start]
+        let next =false;
+        
+        for(let i=0 ;i<num;i++){
+            
+            // start\
+            curpos[0]+=dist[dir][0]
+            curpos[1]+=dist[dir][1]
+          
+            //장애물 이나 공원을 벗어나면 이동하지 않는다.
+            if(validate(curpos[0],curpos[1])){
+                console.log('공원')
+                next=true
+                break;
+            }
+            
+            console.log(curpos,park[curpos[0]][curpos[1]])
+            if(park[curpos[0]][curpos[1]]==='X'){
+                next=true
+                break;
+            }
+            
+            
+        }
+        if(!next){
+        start=curpos
+    }
+          
+
+        // dist.dir
+        
+        
+    })
+    
+    return start;
 }
